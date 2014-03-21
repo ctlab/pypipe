52eeda3810512df776bf1b6c9fd15f6bed5cabb1
semkagtn <semkagtn@gmail.com>
2014-03-21 18:36:35 +0400

    redesigned bowtie2

--- tools/bowtie2.py
+++ tools/bowtie2.py
@@ -4,27 +4,135 @@ import formats
 from pipeline import create_program
 
 
-def bowtie2(x, S, U=None, r1=None, r2=None):
+def bowtie2(x, S, U=None, r1=None, r2=None, q=None, qseq=None, f=None,
+            r=None, c=None, s=None, skip=None, u=None, qupto=None,
+            trim5=None, trim3=None, phred33=None, phred64=None,
+            solexa_quals=None, int_quals=None, very_fast=None,
+            fast=None, sensitive=None, very_sensitive=None,
+            very_fast_local=None, fast_local=None, sensitive_local=None,
+            very_sensitive_local=None):
+    # Checking conflicts
     if (U and r1) or (U and r2) or (not U and not (r1 and r2)):
-        sys.exit("bowtie2: Wrong arguments. Use only 'U' or only 'r1' 'r2'")
+        sys.exit("bowtie2: Wrong options. Use only 'U' or 'r1' and 'r2'")
+    if q and (qseq or f or r or c) or qseq and (
+            f or r or c) or f and (r or c) or r and c:
+        sys.exit("bowtie2: 'q', 'qseq', 'f', 'r', 'c' - this options conflict")
+    if s and skip:
+        sys.exit("bowtie2: use only 'skip' or 's'")
+    if u and qupto:
+        sys.exit("bowtie2: use only 'u' or 'qupto'")
+
+    # Checking types
     if type(x) != formats.Bowtie2Index:
-        sys.exit("bowtie2: x argument must be Bowtie2Index")
+        sys.exit("bowtie2: 'x' argument must be Bowtie2Index")
     if type(S) != str:
-        sys.exit("bowtie2: S argument must be string")
-    if U:
-        if type(U) != formats.Fastq:
-            sys.exit("bowtie2: U argument must be in FASTQ format")
+        sys.exit("bowtie2: 'S' argument must be string")
+    if s and type(s) != int:
+        sys.exit("bowtie2: 's' argument must be int")
+    if skip and type(skip) != int:
+        sys.exit("bowtie2: 'skip' argument must be int")
+    if u and type(u) != int:
+        sys.exit("bowtie2: 'u' argument must be int")
+    if qupto and type(qupto) != int:
+        sys.exit("bowtie2: 'qupto' argument must be int")
+    if trim5 and type(trim5) != int:
+        sys.exit("bowtie2: 'trim5' argument must be int")
+    if trim3 and type(trim3) != int:
+        sys.exit("bowtie2: 'trim3' argument must be int")
+
+    if qseq:
+        read_format = formats.Qseq
+        str_format = "QSEQ"
+    elif f:
+        read_format = formats.Fasta
+        str_format = "FASTA"
+    elif r:
+        read_format = formats.Unknown
+        str_format = "UNKNOWN"
+    elif c:
+        read_format = str
+        str_format = "string"
     else:
-        if type(r1) != formats.Fastq:
-            sys.exit("bowtie2: r1 argument must be in FASTQ format")
-        if type(r2) != formats.Fastq:
-            sys.exit("bowtie2: r2 argument must be in FASTQ format")
+        read_format = formats.Fastq
+        str_format = "FASTQ"
+    error_msg = "bowtie2: U argument must be array of " + str_format
     if U:
-        cmd = ["bowtie2", "-x", x.path, "-U", U.path, "-S", S]
+        for read in U:
+            if type(read) != read_format:
+                sys.exit(error_msg)
     else:
-        cmd = ["bowtie2", "-x", x.path, "-1", r1.path, "-2", r2.path, "-S", S]
+        for read in r1:
+            if type(read) != read_format:
+                sys.exit(error_msg)
+        for read in r2:
+            if type(read) != read_format:
+                sys.exit(error_msg)
+
+    # Generating basic command
     if U:
-        program = create_program(cmd, [U])
+        reads = ",".join([read.path for read in U])
+        cmd = ["bowtie2", "-x", x.path, "-U", reads, "-S", S]
     else:
-        program = create_program(cmd, [r1, r2])
+        reads1 = ",".join([read.path for read in r1])
+        reads2 = ",".join([read.path for read in r2])
+        cmd = ["bowtie2", "-x", x.path, "-1", reads1, "-2", reads2, "-S", S]
+
+    # Adding options
+    if q:
+        cmd.append("-q")
+    elif qseq:
+        cmd.append("--qseq")
+    elif f:
+        cmd.append("-f")
+    elif r:
+        cmd.append("-r")
+    elif c:
+        cmd.append("-c")
+    if s:
+        cmd.append("-s")
+        cmd.append(str(s))
+    elif skip:
+        cmd.append("--skip")
+        cmd.append(str(skip))
+    if u:
+        cmd.append("-u")
+        cmd.append(str(u))
+    elif qupto:
+        cmd.append("--qupto")
+        cmd.append(str(qupto))
+    if trim5:
+        cmd.append("--trim5")
+        cmd.append(str(trim5))
+    if trim3:
+        cmd.append("--trim3")
+        cmd.append(str(trim3))
+    if phred33:
+        cmd.append("--phred33")
+    if phred64:
+        cmd.append("--phred64")
+    if solexa_quals:
+        cmd.append("--solexa-quals")
+    if int_quals:
+        cmd.append("--int-quals")
+    if very_fast:
+        cmd.append("--very-fast")
+    if fast:
+        cmd.append("--fast")
+    if sensitive:
+        cmd.append("--sensitive")
+    if very_sensitive:
+        cmd.append("--very-sensitive")
+    if very_fast_local:
+        cmd.append("--very-fast-local")
+    if fast_local:
+        cmd.append("--fast-local")
+    if sensitive_local:
+        cmd.append("--sensitive-local")
+    if very_sensitive_local:
+        cmd.append("--very-sensitive-local")
+
+    # Creating program
+    print " ".join(cmd)
+    files_array = U and U or r1 + r2
+    program = create_program(cmd, files_array)
     return formats.Sam(S, program)
